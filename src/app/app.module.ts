ab8a00141105b775398907a29965dafd9028b839
Camilo Muñoz <jcamilomunozz@gmail.com>
2018-09-21 09:30:37 -0500

    Routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }          from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+// import { FormsModule }          from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-const appRoutes: Routes = [
-  { path: 'crisis-center', component: CrisisListComponent },
-  { path: 'heroes', component: HeroListComponent },
- 
-  { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
-  { path: '**', component: NotFoundComponent }
-];
-
 @NgModule({
+  imports: [
+    BrowserModule,
+    // FormsModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
     CrisisListComponent,
     HeroListComponent,
     NotFoundComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true }
-    )
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
